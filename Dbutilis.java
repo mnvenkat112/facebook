2ae5f6d95357ecf216867bb478aadd69bc8c22a8
mnvenka112 <mnvenkat112@gmail.com>
2021-12-26 00:27:40 +0530

    ssh uodate

--- Dbutilis.java
+++ Dbutilis.java
@@ -1,5 +1,9 @@
 hi guys git commands are very imp
 we need to learn must
+<<<<<<< HEAD
 
 thiis is good demand technology
 
+=======
+the devops technology is past faced methodology
+>>>>>>> 0ab5ebe9d4da11ff6508b4864db3e35411f4a04a
--- stringutilis.java
+++ stringutilis.java
@@ -1,4 +1,8 @@
 this is upadated
 hi here going to write string utilities
 
+<<<<<<< HEAD
 here once again updating
+=======
+i am going update once again
+>>>>>>> 0ab5ebe9d4da11ff6508b4864db3e35411f4a04a
